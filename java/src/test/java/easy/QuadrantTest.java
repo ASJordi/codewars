b8833938ee830d39c938ee054927931aa3fda47c
Jordi Ayala <jord7as@gmail.com>
2023-06-21 12:46:13 -0600

    feat(java): :sparkles: Add Quadrants problem

---
+++ java/src/test/java/easy/QuadrantTest.java
@@ -0,0 +1,38 @@
+package easy;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+
+public class QuadrantTest {
+
+    private final Quadrant q = new Quadrant();
+
+    @Test
+    public void quadrantShouldBeFirst() {
+        assertThat(q.quadrant(1, 2)).isEqualTo(1);
+        assertThat(q.quadrant(3, 5)).isEqualTo(1);
+    }
+
+    @Test
+    public void quadrantShouldBeSecond() {
+        assertThat(q.quadrant(-10, 100)).isEqualTo(2);
+    }
+
+    @Test
+    public void quadrantShouldBeThird() {
+        assertThat(q.quadrant(-1, -9)).isEqualTo(3);
+    }
+
+    @Test
+    public void quadrantShouldBeFourth() {
+        assertThat(q.quadrant(19, -56)).isEqualTo(4);
+    }
+
+    @Test
+    public void bothParametersShouldNotBeZero() {
+        assertThatThrownBy(() -> q.quadrant(0, 0))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("x and y cannot be 0");
+    }
+}
