1fa7eef0563896b5510fd36a28a88904cc71bf30
Jordi Ayala <jord7as@gmail.com>
2023-06-22 11:24:41 -0600

    feat(java): :sparkles: Add Total amount of points problem

---
+++ java/src/test/java/easy/TotalPointsTest.java
@@ -0,0 +1,36 @@
+package easy;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TotalPointsTest {
+
+    @Test
+    public void ourTeamWinsAllTheMatches() {
+        assertEquals(30, TotalPoints.points(new String[]
+                {"1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"}));
+    }
+
+    @Test
+    public void ourTeamLosesAllGames() {
+        assertEquals(0, TotalPoints.points(new String[]
+                {"0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"}));
+    }
+
+    @Test
+    public void ourTeamWinsHalfOfTheMatches() {
+        assertEquals(15, TotalPoints.points(new String[]
+                {"1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"}));
+    }
+
+    @Test
+    public void randomScenarios() {
+
+        assertEquals(10, TotalPoints.points(new String[]
+                {"1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"}));
+
+        assertEquals(12, TotalPoints.points(new String[]
+                {"1:0","2:0","3:0","4:4","2:2","3:3","1:4","2:3","2:4","3:4"}));
+    }
+
+}
