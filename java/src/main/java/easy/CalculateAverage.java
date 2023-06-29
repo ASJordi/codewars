03d24082061b175ef6af4287d18576cc00b3c8b3
Jordi Ayala <jord7as@gmail.com>
2023-06-29 11:00:37 -0600

    feat(java): :sparkles: Add Calculate average problem

---
+++ java/src/main/java/easy/CalculateAverage.java
@@ -0,0 +1,23 @@
+package easy;
+
+import java.util.Arrays;
+
+public class CalculateAverage {
+
+    public static double find_average(int[] array) {
+
+//        if (array.length == 0) return 0;
+//
+//        double sum = 0;
+//
+//        for (int i : array) {
+//            sum += i;
+//        }
+//
+//        return sum /= array.length;
+
+        return Arrays.stream(array).average().orElse(0);
+
+    }
+
+}
