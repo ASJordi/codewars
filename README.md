6dd95634626a04218fb61700a488640ede18a08d
Jordi Ayala <jord7as@gmail.com>
2023-07-01 10:34:34 -0600

    feat(java): :sparkles: Add Remove First and Last Character problem

--- README.md
+++ README.md
@@ -63,6 +63,7 @@ Cada lenguaje tiene su propia carpeta en el repositorio. Dentro de cada carpeta,
         <li><a href="/java/src/main/java/easy/OppositeNumber.java">Opposite number</a></li>
         <li><a href="/java/src/main/java/easy/DoubleChar.java">Double Char</a></li>
         <li><a href="/java/src/main/java/easy/CountTheMonkeys.java">Count the Monkeys!</a></li>
+        <li><a href="/java/src/main/java/easy/RemoveFirstAndLastCharacter.java">Remove First and Last Character</a></li>
       </ol>
     </details>
     <details>
---
+++ java/src/main/java/easy/RemoveFirstAndLastCharacter.java
@@ -0,0 +1,31 @@
+package easy;
+
+public class RemoveFirstAndLastCharacter {
+
+    public static void main(String[] args) {
+        String str = "eloquent";
+        StringBuilder word = new StringBuilder();
+        String[] strArray = str.split("");
+
+        for (int i = 0; i < strArray.length; i++) {
+            if ((i == 0) || (i == strArray.length - 1)) continue;
+            word.append(strArray[i]);
+        }
+
+        System.out.println(word);
+    }
+
+    public static String remove(String str) {
+
+        StringBuilder word = new StringBuilder();
+        char[] strArray = str.toCharArray();
+
+        for (int i = 1; i < strArray.length - 1; i++) {
+            word.append(strArray[i]);
+        }
+
+        return word.toString();
+//         return str.substring(1, str.length() - 1);
+    }
+
+}
