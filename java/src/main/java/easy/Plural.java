79fa8e9a09a9e0c5011d95edae13728a421e69cb
Jordi Ayala <jord7as@gmail.com>
2023-07-13 10:54:23 -0600

    feat(java): :sparkles: Add Plural problem

---
+++ java/src/main/java/easy/Plural.java
@@ -0,0 +1,9 @@
+package easy;
+
+public class Plural {
+
+    public static boolean isPlural(float f){
+        return f != 1;
+    }
+
+}
---
+++ java/src/test/java/easy/PluralTest.java
@@ -0,0 +1,16 @@
+package easy;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PluralTest {
+
+    @Test
+    public void BasicTest() {
+        assertEquals(true, Plural.isPlural(0f));
+        assertEquals(true, Plural.isPlural(0.5f));
+        assertEquals(false, Plural.isPlural(1f));
+        assertEquals(true, Plural.isPlural(100f));
+    }
+
+}
