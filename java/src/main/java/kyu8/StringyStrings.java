efcd54aae223ee34f371043e25eb301cff070810
Jordi Ayala <jord7as@gmail.com>
2023-07-18 10:26:04 -0600

    feat(java): :sparkles: Add Stringy Strings problem

---
+++ java/src/main/java/kyu8/StringyStrings.java
@@ -0,0 +1,21 @@
+package kyu8;
+
+public class StringyStrings {
+
+    public static String stringy(int size) {
+
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < size; i++) {
+            if (i % 2 == 0){
+                res.append("1");
+            } else{
+                res.append("0");
+            }
+        }
+
+        return res.toString();
+
+    }
+
+}
