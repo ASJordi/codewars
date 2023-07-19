0cc2fb0b2ba5f3b3152f00c4a59a11d8896e5fc2
Jordi Ayala <jord7as@gmail.com>
2023-07-19 10:26:05 -0600

    feat(java): :sparkles: Add Unfinished Loop problem

---
+++ java/src/main/java/kyu8/UnfinishedLoop.java
@@ -0,0 +1,18 @@
+package kyu8;
+
+import java.util.*;
+
+public class UnfinishedLoop {
+
+    public static List CreateList(int number) {
+        List list = new ArrayList();
+
+        for(int count = 1; count <= number; count++)
+        {
+            list.add(count);
+        }
+
+        return list;
+    }
+
+}
