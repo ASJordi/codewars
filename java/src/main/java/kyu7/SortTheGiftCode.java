6f9b387e7ccc28b6e796151157a91ad2f636ce6c
Jordi Ayala <jord7as@gmail.com>
2023-07-24 11:22:44 -0600

    feat(java): :sparkles: Add Sort the Gift Code problem

---
+++ java/src/main/java/kyu7/SortTheGiftCode.java
@@ -0,0 +1,13 @@
+package kyu7;
+
+import java.util.Arrays;
+
+public class SortTheGiftCode {
+
+    public String sortGiftCode(String code){
+        char[] codeArr = code.toCharArray();
+        Arrays.sort(codeArr);
+        return new String(codeArr);
+    }
+
+}
