f027cd6b4e433329c7dd737c97a5d7ef80aefc9b
Jordi Ayala <jord7as@gmail.com>
2023-08-11 10:51:53 -0600

    feat(java): :sparkles: Add Shortest Word problem

---
+++ java/src/main/java/kyu7/ShortestWord.java
@@ -0,0 +1,19 @@
+package kyu7;
+
+public class ShortestWord {
+
+    public static int findShort(String s) {
+        String[] words = s.split(" ");
+
+        String shortest = words[0];
+
+        for(String str : words) {
+            if (str.length() < shortest.length()) {
+                shortest = str;
+            }
+        }
+
+        return shortest.length();
+    }
+
+}
