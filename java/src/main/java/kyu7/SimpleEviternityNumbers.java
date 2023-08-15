ffd9ab27bfe4e55a453687d5d3b4a038d2fba863
Jordi Ayala <jord7as@gmail.com>
2023-08-15 09:59:51 -0600

    feat(java): :sparkles: Add Simple eviternity numbers problem

---
+++ java/src/main/java/kyu7/SimpleEviternityNumbers.java
@@ -0,0 +1,36 @@
+package kyu7;
+
+import java.util.*;
+
+public class SimpleEviternityNumbers {
+
+    public static int solve(int a, int b){
+        int count = 0;
+
+        for (int i = a; i < b; i++) {
+            if (isEviternityNumber(i)) count++;
+        }
+
+        return count;
+    }
+
+    public static boolean isEviternityNumber(int n) {
+        ArrayList<String> validNumbers = new ArrayList<>(Arrays.asList("8", "5", "3"));
+        String num = String.valueOf(n);
+        int numLen = num.length();
+
+        for (int i = 0; i < num.length(); i++) {
+            String digit = Character.toString(num.charAt(i));
+            if (!validNumbers.contains(digit)) return false;
+        }
+
+        int countEight = numLen - num.replace("8", "").length();
+        int countFive = numLen - num.replace("5", "").length();
+        int countThree = numLen - num.replace("3", "").length();
+
+        if (countEight == 0) return false;
+
+        return (countEight >= countFive) && (countFive >= countThree);
+    }
+
+}
---
+++ java/src/test/java/kyu7/SimpleEviternityNumbersTest.java
@@ -0,0 +1,49 @@
+package kyu7;
+
+import org.junit.Test;
+import java.util.Random;
+import static org.junit.Assert.assertEquals;
+
+public class SimpleEviternityNumbersTest {
+
+    private static Random random = new Random();
+
+    @Test
+    public void basicTests(){
+        assertEquals(4, SimpleEviternityNumbers.solve(1,100));
+        assertEquals(14, SimpleEviternityNumbers.solve(1,1000));
+        assertEquals(37, SimpleEviternityNumbers.solve(1,10000));
+        assertEquals(103, SimpleEviternityNumbers.solve(1,100000));
+    }
+
+    private static int random(int l, int u){
+        return random.nextInt(u-l)+l;
+    }
+
+    private static int lop9(int a, int b){
+        int c = 0;
+        while (a < b){
+            int x = 0, y = 0, z = 0, flag = 1;
+            char[] t = String.valueOf(a).toCharArray();
+            for (char ch : t){
+                if (ch == '8') x++;
+                else if (ch == '5') y++;
+                else if (ch == '3') z++;
+                else flag = 0;
+            }
+            if (x >= y && y >= z && flag == 1) c++;
+            a++;
+        }
+        return c;
+    }
+
+    @Test
+    public void randomTests(){
+        for(int i=0;i<100;i++){
+            int a = random(1,100);
+            int b = random(a+100,499999);
+            assertEquals(lop9(a,b), SimpleEviternityNumbers.solve(a,b));
+        }
+    }
+
+}
