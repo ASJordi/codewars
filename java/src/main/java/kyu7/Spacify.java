00528fc3d484c15842dad7ed393eb0dcf9a75628
Jordi Ayala <jord7as@gmail.com>
2023-08-19 08:36:26 -0600

    feat(java): :sparkles: Add Spacify problem

---
+++ java/src/main/java/kyu7/Spacify.java
@@ -0,0 +1,15 @@
+package kyu7;
+
+public class Spacify {
+
+    public static String spacify(String str){
+        StringBuilder word = new StringBuilder();
+
+        for (char c : str.toCharArray()){
+            word.append(c).append(" ");
+        }
+
+        return word.toString().trim();
+    }
+
+}
