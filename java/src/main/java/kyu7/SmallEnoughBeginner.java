314810dbb8c5688502e469b23fb669b4cfc63e2a
Jordi Ayala <jord7as@gmail.com>
2023-08-19 08:47:28 -0600

    feat(java): :sparkles: Add Small enough? - Beginner problem

---
+++ java/src/main/java/kyu7/SmallEnoughBeginner.java
@@ -0,0 +1,10 @@
+package kyu7;
+
+public class SmallEnoughBeginner {
+
+    public static boolean smallEnough(int[] a, int limit){
+        for (int num : a) if (num > limit) return false;
+        return true;
+    }
+
+}
