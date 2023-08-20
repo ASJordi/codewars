99ce36a45320d31b93d7f336df052a3e999bd441
Jordi Ayala <jord7as@gmail.com>
2023-08-20 12:32:48 -0600

    feat(java): :sparkles: Add WASD Movement problem

---
+++ java/src/main/java/kyu7/GameInput.java
@@ -0,0 +1,17 @@
+package kyu7;
+
+public class GameInput {
+
+    public float[] getVector(boolean[] wasd) {
+        float positionX = 0f;
+        float positionY = 0f;
+
+        if (wasd[0]) positionY++;
+        if (wasd[3]) positionX++;
+        if (wasd[2]) positionY--;
+        if (wasd[1]) positionX--;
+
+        return positionX != 0 && positionY != 0 ? new float[] {positionX * .70710677f, positionY * .70710677f} : new float[] {positionX, positionY};
+    }
+
+}
