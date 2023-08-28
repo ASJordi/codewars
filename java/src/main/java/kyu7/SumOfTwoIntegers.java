6fd26203509e1724d3712545661c5f674e30caa5
Jordi Ayala <jord7as@gmail.com>
2023-08-28 11:33:13 -0600

    feat(java): :sparkles: Add Sum of Two Integers problem

---
+++ java/src/main/java/kyu7/SumOfTwoIntegers.java
@@ -0,0 +1,16 @@
+package kyu7;
+
+public class SumOfTwoIntegers {
+
+    public static int add(int x, int y) {
+        while (y != 0) {
+
+            int carry = x & y;
+            x = x ^ y;
+            y = carry << 1;
+        }
+
+        return x;
+    }
+
+}
---
+++ java/src/test/java/kyu7/SumOfTwoIntegersTest.java
@@ -0,0 +1,29 @@
+package kyu7;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SumOfTwoIntegersTest {
+
+    @Test
+    public void checkPositiveValues() {
+        assertEquals( 3, SumOfTwoIntegers.add(1,2));
+        assertEquals(24, SumOfTwoIntegers.add(5,19));
+        assertEquals(40, SumOfTwoIntegers.add(23,17));
+    }
+
+    @Test
+    public void checkNegativeValues() {
+        assertEquals( -30, SumOfTwoIntegers.add(-14,-16));
+        assertEquals(-226, SumOfTwoIntegers.add(-50,-176));
+        assertEquals( -39, SumOfTwoIntegers.add(-10,-29));
+    }
+
+    @Test
+    public void checkMixtureValues() {
+        assertEquals(  0, SumOfTwoIntegers.add(-13,13));
+        assertEquals( -9, SumOfTwoIntegers.add(-27,18));
+        assertEquals(-60, SumOfTwoIntegers.add(-90,30));
+    }
+
+}
