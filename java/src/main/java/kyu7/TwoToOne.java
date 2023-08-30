140f2092c99851fa5ca3380e4141eb377e3d6ccc
Jordi Ayala <jord7as@gmail.com>
2023-08-30 12:31:27 -0600

    feat(java): :sparkles: Add Two to One problem

---
+++ java/src/main/java/kyu7/TwoToOne.java
@@ -0,0 +1,24 @@
+package kyu7;
+
+import java.util.*;
+
+public class TwoToOne {
+
+    public static String longest (String s1, String s2) {
+        StringBuilder words = new StringBuilder();
+        StringBuilder newWord = new StringBuilder();
+        words.append(s1).append(s2);
+        LinkedList<Character> letters = new LinkedList<>();
+
+        for (char c : words.toString().toCharArray()){
+            if (!letters.contains(c)) letters.add(c);
+        }
+
+        Collections.sort(letters);
+
+        for (char l : letters) newWord.append(l);
+
+        return newWord.toString();
+    }
+
+}
