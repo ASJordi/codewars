a198d9587f6f3661a0b4f8fb3702cf1d3d62e191
Jordi Ayala <jord7as@gmail.com>
2023-08-31 09:52:43 -0600

    feat(java): :sparkles: Add ANTISTRING problem

--- README.md
+++ README.md
@@ -357,6 +357,7 @@ Cada lenguaje tiene su propia carpeta en el repositorio. Dentro de cada carpeta,
         <li><a href="/java/src/main/java/kyu7/ZebulansNightmare.java">Zebulans Nightmare</a></li>
         <li><a href="/java/src/main/java/kyu7/Quicksum.java">Quicksum</a></li>
         <li><a href="/java/src/main/java/kyu7/ValidSpacing.java">Valid Spacing</a></li>
+        <li><a href="/java/src/main/java/kyu7/AntiString.java">ANTISTRING</a></li>
       </ol>
     </details>
     <details>
---
+++ java/src/main/java/kyu7/AntiString.java
@@ -0,0 +1,33 @@
+package kyu7;
+
+public class AntiString {
+
+    public static String antiString(String str){
+        StringBuilder reverseNumbers = new StringBuilder("9876543210");
+        StringBuilder reverseAlphabet = new StringBuilder("zyxwvutsrqponmlkjihgfedcba");
+        StringBuilder alphabet = new StringBuilder("abcdefghijklmnopqrstuvwxyz");
+        StringBuilder antiString = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (Character.isLetter(c)){
+                if (Character.isLowerCase(c)){
+                    String l = String.valueOf(reverseAlphabet.charAt(alphabet.indexOf(String.valueOf(c))));
+                    antiString.append(l.toUpperCase());
+                } else {
+                    String temp = String.valueOf(c);
+                    String l = String.valueOf(reverseAlphabet.charAt(alphabet.indexOf(temp.toLowerCase())));
+                    antiString.append(l);
+                }
+            }
+
+            if (Character.isDigit(c)){
+                antiString.append(reverseNumbers.charAt(Integer.parseInt(Character.toString(c))));
+            }
+        }
+
+        return antiString.reverse().toString();
+    }
+
+}
