f53212969b73e8258d6e75dedc44657a919518bb
Jordi Ayala <jord7as@gmail.com>
2023-09-06 07:05:46 -0600

    feat(java): :sparkles: Add Very Even Numbers problem

---
+++ java/src/main/java/kyu7/VeryEvenNumbers.java
@@ -0,0 +1,18 @@
+package kyu7;
+
+public class VeryEvenNumbers {
+
+    public static boolean isVeryEvenNumber(int number) {
+        if (number < 10) return number % 2 == 0;
+
+        while (number >= 10){
+            String[] digits = String.valueOf(number).split("");
+            int sum = 0;
+            for (String digit : digits) sum += Integer.parseInt(digit);
+            number = sum;
+        }
+
+        return number % 2 == 0;
+    }
+
+}
---
+++ java/src/test/java/kyu7/VeryEvenNumbersTest.java
@@ -0,0 +1,23 @@
+package kyu7;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class VeryEvenNumbersTest {
+
+    @Test
+    public void sampleTests() {
+        assertEquals(true, VeryEvenNumbers.isVeryEvenNumber(0));
+        assertEquals(true, VeryEvenNumbers.isVeryEvenNumber(4));
+        assertEquals(false, VeryEvenNumbers.isVeryEvenNumber(12));
+        assertEquals(true, VeryEvenNumbers.isVeryEvenNumber(222));
+        assertEquals(false, VeryEvenNumbers.isVeryEvenNumber(5));
+        assertEquals(false, VeryEvenNumbers.isVeryEvenNumber(45));
+        assertEquals(false, VeryEvenNumbers.isVeryEvenNumber(4554));
+        assertEquals(false, VeryEvenNumbers.isVeryEvenNumber(1234));
+        assertEquals(false, VeryEvenNumbers.isVeryEvenNumber(88));
+        assertEquals(true, VeryEvenNumbers.isVeryEvenNumber(24));
+        assertEquals(true, VeryEvenNumbers.isVeryEvenNumber(400000220));
+    }
+
+}
