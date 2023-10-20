4b27f760e563f9a4002a90a53605a8cdda20fda4
Jordi Ayala <jord7as@gmail.com>
2023-10-20 09:15:01 -0600

    feat(java): :sparkles: Add Cryptanalysis Word Patterns problem

---
+++ java/src/main/java/kyu7/CryptanalysisWordPatterns.java
@@ -0,0 +1,25 @@
+package kyu7;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CryptanalysisWordPatterns {
+
+    public static String wordPattern(String word){
+        Map<Character, Integer> map = new HashMap<>();
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        word = word.toLowerCase();
+
+        for (char w : word.toCharArray()){
+            if (!map.containsKey(w)){
+                map.put(w, count);
+                result.append(map.get(w)).append(".");
+                count++;
+            } else result.append(map.get(w)).append(".");
+        }
+
+        return result.substring(0, result.length() - 1);
+    }
+
+}
---
+++ java/src/test/java/kyu7/CryptanalysisWordPatternsTest.java
@@ -0,0 +1,20 @@
+package kyu7;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CryptanalysisWordPatternsTest {
+
+    @Test
+    public void exampleTests() {
+        String[][] tests = {
+                {"hello", "0.1.2.2.3"},
+                {"heLlo", "0.1.2.2.3"},
+                {"helLo", "0.1.2.2.3"},
+                {"Hippopotomonstrosesquippedaliophobia", "0.1.2.2.3.2.3.4.3.5.3.6.7.4.8.3.7.9.7.10.11.1.2.2.9.12.13.14.1.3.2.0.3.15.1.13"},
+        };
+
+        for(String[] arr: tests) assertEquals(arr[1], CryptanalysisWordPatterns.wordPattern(arr[0]));
+    }
+
+}
